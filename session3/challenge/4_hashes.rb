5a2a2ee01af92164c4c23d4cd496e12f25ef10dc
Dylan Rhodius <dylanrhodius@hotmail.com>
2016-12-01 15:26:40 +0000

    Finished 4

--- session3/challenge/4_hashes.rb
+++ session3/challenge/4_hashes.rb
@@ -5,3 +5,19 @@
 # There will be no punctuation in the strings.
 #
 # first_pos "The dog and the cat and the cow" # => {"The" => 0, "dog" => 1, "and" => 2, "the" => 3, "cat" => 4, "cow" => 7}
+
+public
+
+def first_pos(string)
+  word_array = string.split(" ")
+  count_hash = {}
+
+  word_array.each_with_index do |word, index|
+    count_hash[word] ||= index
+  end
+
+ count_hash
+
+end
+
+first_pos "The dog and the cat and the cow" # => {"The" => 0, "dog" => 1, "and" => 2, "the" => 3, "cat" => 4, "cow" => 7}
