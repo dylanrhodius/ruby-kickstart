fdaf1f4917ded7630396b708fe230c3138509ec2
Dylan Rhodius <dylanrhodius@hotmail.com>
2016-12-01 10:12:15 +0000

    Added ex13, could not finish it by myself, left my original answer. Looked at author's answer to understand problem

--- session2/challenge/13_classes_iterating_sorting.rb
+++ session2/challenge/13_classes_iterating_sorting.rb
@@ -1,36 +1,85 @@
-# Define a class called User that keeps track of a person's username
-# it should receive the username when initialized
-# it should have a method add_blog which accepts a date and text
-# it should have a method blogs which returns an array of all blogs the user has written
-# they should be in reverse chronological order (newest first)
-#
-# Define a class called Blog that could be used to store an entry for your web log.
-# The class should have a getter and setter methods: text , date , user
-# its initialize method should receive the date, user , and text
-# have a method called summary that returns the first 10 words from the text (or the entire text if it is less than 10 words)
-#
-# Two blogs should be equal to eachother if they have the same user, date, and text
-# here is a partially filled out example of how to define the == operator:
-#      def ==(other)
-#        return self.date == other.date
-#      end
+#looking at errors and line 101 realised blog entry
+#needs to return: user, date and text
+#tests suggest method needs to be specifically called
+#entry
+#tests suggest no username variable:
+#wasn't pointing to USER CLASS!!!
+#added "\n" after date
+#removed the space after "\n"
+#6 examples passed so far
+#had to copy def=other method, did not understand.
+#corrected add_blog, had to push instance of class blog
+#Can't pass example 9 "undefined method date" on line
+#60. But it does pass example 6 which uses this line..
+
+require 'date'
+
+class User
+  attr_accessor :username
+
+  def initialize(username)
+    @username = username
+    @blogs_written_array = []
+  end
+
+  def add_blog(date, text)
+    to_add = Blog.new(date, self, text)
+    @blogs_written_array.push(to_add)
+
+  end
+
+  def blogs
+    @blogs_written_array.sort!.reverse!
+  end
+
+end
 
 
+class Blog
+  attr_accessor :text, :date, :user
+
+  def initialize(date, user, text)
+    self.date = date
+    self.user = user
+    self.text = text
+  end
+
+  def entry
+    return "#{user.username} #{date}\n#{text}"
+  end
+
+  def summary
+    summary_array = text.split(" ")
+    if summary_array.length > 10
+      summary_array[0...10].join(" ")
+    else
+      summary_array.join(" ")
+    end
+  end
+
+  def ==(other)
+    date == other.date &&
+    user == other.user &&
+    text == other.text
+  end
+
+
+end
 
 # ==========  EXAMPLE  ==========
 #
-# lissa = User.new 'QTSort'
-# lissa.username                  # => "QTSort"
+#lissa = User.new 'QTSort'
+#lissa.username                  # => "QTSort"
 # lissa.blogs                     # => []
 #
-# lissa.add_blog Date.parse("2010-05-28") , "Sailor Mars is my favourite"
+#lissa.add_blog Date.parse("2010-05-28") , "Sailor Mars is my favourite"
 # lissa.blogs                     # => [ blog1 ]
 #
-# blog1 = lissa.blogs.first
-# blog1.user                      # => lissa
+#blog1 = lissa.blogs.first
+#blog1.user                      # => lissa
 #
-# Blog.new Date.parse("2007-01-02"), lissa, "Going dancing!"                                    # we'll call this blog2
-# Blog.new Date.parse("2006-01-02"), lissa, "For the last time, fuck facebook >.<"              # we'll call this blog3
+#Blog.new Date.parse("2007-01-02"), lissa, "Going dancing!"                                    # we'll call this blog2
+#Blog.new Date.parse("2006-01-02"), lissa, "For the last time, fuck facebook >.<"              # we'll call this blog3
 # Blog.new Date.parse("2010-01-02"), lissa, "Got a new job, cuz I'm pretty much the best ^_^"   # we'll call this blog4
 # lissa.blogs                     # => [ blog1 , blog4 , blog2 , blog3 ]
 #
@@ -58,9 +107,3 @@
 #                       From the school of revision, Comes the standard inventor's rule, Books of subtle notation Compositions, all original
 #                       I am a pioneer, synthetic engineer, On the brink of discovery, On the eve of historic light, Worked in secret for decades,
 #                       All my labor will be lost with time
-
-
-
-# date docs are at: http://ruby-doc.org/core/classes/Date.html
-# don't spend too much time worrying about them :)
-require 'date'
