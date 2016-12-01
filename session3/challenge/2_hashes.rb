fd016645fb9a1affb3b03e08b5dbee77b161622f
Dylan Rhodius <dylanrhodius@hotmail.com>
2016-12-01 15:03:11 +0000

    Finished challenge 2 by getting complete help by Katerina

--- session3/challenge/2_hashes.rb
+++ session3/challenge/2_hashes.rb
@@ -1,30 +1,28 @@
-# Given a nonnegative integer, return a hash whose keys are all the odd nonnegative integers up to it
-# and each key's value is an array containing all the even non negative integers up to it.
-#
-# Examples:
-# staircase 1  # => {1 => []}
-# staircase 2  # => {1 => []}
-# staircase 3  # => {1 => [], 3 => [2]}
-# staircase 4  # => {1 => [], 3 => [2]}
-# staircase 5  # => {1 => [], 3 => [2], 5 =>[2, 4]}
+# completed code looking at Katerina's code.
+# walked me through a previous solution of hers.
+# once again, thank you! I would not have been able
+# to do it without her reference.
 
 def staircase(n)
- stair_hash = Hash.new
- stair_array = Array.new
+  stair_hash = {}
 
- for i in 1..n
-   puts "i = #{i}"
-   if i.odd?
-     stair_hash[:i] = stair_array
-     puts "Our hash: #{stair_hash}"
-     puts "Our array: #{stair_array}"
-   else
-     stair_array<<i if i < stair_hash[i]
-     puts "Our hash: #{stair_hash}"
-     puts "Our array: #{stair_array}"
-   end
- end
- stair_hash
+# create a for loop from 1 to parameter passed.
+  for i in 1..n
+    puts "i = #{i}"
+    if i.odd?
+      stair_array = []
+# if this parameter is odd, create a new empty array
+      for j in 1..i
+        if j.even?
+          stair_array<<j
+# loop again, this time checking if numbers from 1 to i are even. If so, push even values to the newly created array.
+        end
+      end
+      stair_hash[i] = stair_array
+#finally, return each key of i which is odd, with its respective array.
+    end
+  end
+  stair_hash
 end
 
 puts "staircase 1: #{staircase 1}"  # => {1 => []}
@@ -32,6 +30,7 @@ puts ""
 puts "staircase 2: #{staircase 2}"  # => {1 => []}
 puts ""
 puts "staircase 3: #{staircase 3}"  # => {1 => [], 3 => [2]}
+
 puts ""
 puts "staircase 4: #{staircase 4}"  # => {1 => [], 3 => [2]}
 puts ""
